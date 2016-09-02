58f5f01f245ec2e9a7523f80f26abf3f9e42a890
wenjun.zhong <wenjun.zhong@tcl.com>
2016-09-02 14:08:44 +0800

    add get database method

---
+++ getDatabases4.bat
@@ -0,0 +1,3 @@
+adb shell "run-as com.tcl.mie.launcher.lite chmod 666 /data/data/com.tcl.mie.launcher.lite/databases/launcher.db"
+
+adb pull data/data/com.tcl.mie.launcher.lite/databases/launcher.db
---
+++ getDatabases5.bat
@@ -0,0 +1,2 @@
+adb exec-out run-as com.tcl.mie.launcher.lite cat databases/launcher.db > launcher.db
+pause
